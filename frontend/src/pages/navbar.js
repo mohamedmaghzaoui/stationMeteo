e2471bfa82aaf4e9c662c3a4869fbdf16f522bc6
Mohamed Maghzaoui <mohamed.maghzaoui53@gmail.com>
2024-01-31 14:41:21 +0100

    working on home page

--- frontend/src/pages/navbar.js
+++ frontend/src/pages/navbar.js
@@ -8,12 +8,10 @@ import { useState } from "react";
 export const Navbar = () => {
   const [theme, setTheme] = useState("light");
   return (
-    <div className=""  >
+    <div className="">
       <nav className="navbar navbar-expand-lg bg-body-tertiary navbar bg-primary">
         <div className="container-fluid">
-          <a className="navbar-brand">
-            Station
-          </a>
+          <a className="navbar-brand">Station</a>
           <button
             className="navbar-toggler"
             type="button"
@@ -30,34 +28,42 @@ export const Navbar = () => {
             id="navbarSupportedContent"
           >
             <ul className="navbar-nav me-auto mb-2 mb-lg-0 container-fluid">
-              <li className="nav-item">
-                <Link className="nav-link" to={"/"}>
+              <li className="nav-item  px-1">
+                <Link className="nav-link fw-medium " to={"/"}>
                   Home
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/Sensor"}>
+
+              <li className="nav-item px-1">
+                <Link className="nav-link fw-medium" to={"/Sensor"}>
                   Sensor
                 </Link>
               </li>
-              <li className="nav-item">
-              <div class="dropdown">
-  <button class="nav-link dropdown-toggle"  data-bs-toggle="dropdown" aria-expanded="false">
-    Account
-  </button>
-  <ul class="dropdown-menu">
-    <li>
-      <Link className="dropdown-item" to={"/Login"}>Login </Link>
-      </li>
-      <li>
-      <Link className="dropdown-item" to={"/SignUp"}>Sign Up </Link>
-      </li>
-
-  </ul>
-</div>
+              <li className="nav-item px-xl-1 px-lg-1">
+                <div class="dropdown">
+                  <button
+                    class="nav-link dropdown-toggle fw-medium"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Account
+                  </button>
+                  <ul class="dropdown-menu ">
+                    <li>
+                      <Link className="dropdown-item " to={"/Login"}>
+                        Login{" "}
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to={"/SignUp"}>
+                        Sign Up{" "}
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link">API Docs</Link>
+              <li className="nav-item col-xl-1 col-lg-1 col-4">
+                <Link className="nav-link fw-medium">API Docs</Link>
               </li>
               {/*dropdown begin */}
               <li className="nav-item dropdown  offset-xl-8  ">
@@ -82,16 +88,15 @@ export const Navbar = () => {
                 <ul className="dropdown-menu">
                   <li>
                     <a
-                      onClick={() => 
-                        {setTheme("light")
-                        document.querySelector("body").setAttribute("data-bs-theme","light")
-                        document.querySelector("body").style.backgroundColor = "#f5f5f5";
-                        
-
-                    
-                    }}
+                      onClick={() => {
+                        setTheme("light");
+                        document
+                          .querySelector("body")
+                          .setAttribute("data-bs-theme", "light");
+                        document.querySelector("body").style.backgroundColor =
+                          "#f5f5f5";
+                      }}
                       className="dropdown-item"
-
                     >
                       <FontAwesomeIcon cl icon={faSun} size="1x" />{" "}
                       <span className="mx-1">Light</span>
@@ -100,12 +105,13 @@ export const Navbar = () => {
                   <li>
                     <a
                       onClick={() => {
-                        setTheme("dark")
-                        document.querySelector("body").setAttribute("data-bs-theme","dark")
-                        document.querySelector("body").style.backgroundColor = "#343a40";
-
-
-                    }}
+                        setTheme("dark");
+                        document
+                          .querySelector("body")
+                          .setAttribute("data-bs-theme", "dark");
+                        document.querySelector("body").style.backgroundColor =
+                          "#343a40";
+                      }}
                       className="dropdown-item  "
                     >
                       <FontAwesomeIcon
@@ -121,7 +127,6 @@ export const Navbar = () => {
           </div>
         </div>
       </nav>
-      
     </div>
   );
 };
