28d97b37d8f41dbcd8da3ff7d1fb9c7064a65bc1
mohamedmaghzaoui <mohamedmaghzaoui53@gmail.com>
2024-05-14 11:14:36 +0200

    added user email verification

--- stationServer/src/Controller/UserController.php
+++ stationServer/src/Controller/UserController.php
@@ -14,6 +14,24 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class UserController extends AbstractController
 {
+    //function to verify user email for firest react registration form
+    #[Route('/users/verify', name: 'users/verify')]
+    public function verifyUser(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $repository = $doctrine->getRepository(User::class);
+
+        // Get data from the request
+        $data = json_decode($request->getContent(), true);
+
+        // Check if the email already exists
+        $existingUser = $repository->findOneBy(['email' => $data['email']]);
+        if ($existingUser) {
+            return $this->json(['Email already exists'], Response::HTTP_CONFLICT);
+        }
+
+        return $this->json(["user doesnt not exist"]);
+    }
+
     #[Route('/users', name: "users")]
     public function addUser(Request $request, ManagerRegistry $doctrine, UserPasswordHasherInterface $passwordHasher): Response
     {
