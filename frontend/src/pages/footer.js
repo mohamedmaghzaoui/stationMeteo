b4377de6326655e45c315afa69a89235a790e772
mohamedmaghzaoui <mohamedmaghzaoui53@gmail.com>
2024-05-19 02:27:38 +0200

    working on footer

--- frontend/src/pages/footer.js
+++ frontend/src/pages/footer.js
@@ -1,7 +1,67 @@
+import { Link } from "react-router-dom";
+
 export const Footer = () => {
   return (
     <div>
-      <h1>footer</h1>
+      <h4 style={{ color: "#5D6778" }}>Developpers</h4>
+      <div className="row">
+        <div className="col-1">
+          <Link
+            style={{ textDecoration: "underline" }}
+            className="nav-link my-1 col"
+            to={"/price"}
+          >
+            Price
+          </Link>
+          <Link
+            style={{ textDecoration: "underline" }}
+            className="nav-link my-1"
+            to={"/price"}
+          >
+            Sensor
+          </Link>
+          <Link
+            style={{ textDecoration: "underline" }}
+            className="nav-link my-1"
+            to={"/price"}
+          >
+            Account
+          </Link>
+          <Link
+            style={{ textDecoration: "underline" }}
+            className="nav-link "
+            to={"/price"}
+          >
+            Github
+          </Link>
+        </div>
+        <div className=" col">
+          <Link
+            style={{ textDecoration: "underline" }}
+            className="nav-link my-1"
+            to={"/price"}
+          >
+            About us
+          </Link>
+          <Link
+            style={{ textDecoration: "underline" }}
+            className="nav-link my-1"
+            to={"/price"}
+          >
+            Contact us
+          </Link>
+          <Link
+            style={{ textDecoration: "underline" }}
+            className="nav-link my-1"
+            to={"/price"}
+          >
+            Privacy
+          </Link>
+        </div>
+      </div>
+
+      <p></p>
+      <p></p>
     </div>
   );
 };
