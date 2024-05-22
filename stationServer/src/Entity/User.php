f0f2f5cc7c2eaad6941b72c7b73d30bd66d16837
mohamedmaghzaoui <mohamedmaghzaoui53@gmail.com>
2024-05-23 01:26:50 +0200

    fixed email issue

--- stationServer/src/Entity/User.php
+++ stationServer/src/Entity/User.php
@@ -29,6 +29,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->sensors = new ArrayCollection();
         $this->passwordHasher = $passwordHasher;
     }
+    #[Orm\Column(length: 255, unique: true)]
     private ?string $email = null;
 
 
